a698ddc9eb961f5eeb02df4ca124ce2428d990e7
Roberto Vázquez <rvazquez@orioncid.mx>
2019-01-16 16:59:20 -0600

    Carga inicial del proyecto

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Security;
+using System.Security.Permissions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+
+namespace LoopPanelSample
+{
+	public partial class App: Application
+	{
+	}
+}
---
+++ AppWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using DrWPF.Windows.Controls;
+using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace LoopPanelSample
+{
+
+    //b     k   s
+    //c     l   t
+    //d     m   v
+    //f     n   w
+    //g     p   x
+    //h     q   y
+    //j     r   z
+
+    public partial class AppWindow
+    {
+        Random rand = new Random();
+        Storyboard sb = new Storyboard();
+        DoubleAnimation slotAnimation = new DoubleAnimation();
+        double? animation_from = 0;
+
+        public AppWindow()
+        {
+            this.InitializeComponent();
+
+            Init();
+        }
+
+        void OnToolBarButtonClick(object sender, RoutedEventArgs e)
+        {
+            //int index = 3; // int.Parse((e.OriginalSource as Button).Content.ToString());
+            //llb.SelectedIndex = index;
+            //(llb.ItemContainerGenerator.ContainerFromItem(llb.Items[index]) as ListBoxItem).Focus();
+
+            RunSlot();
+        }
+
+        private void Init()
+        {
+            slotAnimation.EasingFunction = new CubicEase() { EasingMode = EasingMode.EaseInOut };
+            slotAnimation.RepeatBehavior = new RepeatBehavior(1);
+            slotAnimation.Completed += SlotAnimation_Completed;
+
+            Storyboard.SetTarget(slotAnimation, llb);
+            Storyboard.SetTargetProperty(slotAnimation, new PropertyPath(LoopingListBox.OffsetProperty));
+
+            sb = new Storyboard();
+            sb.Children.Add(slotAnimation);
+        }
+        private void RunSlot()
+        {
+            var distance = rand.Next(4, 13);
+            var duration = (distance / 3) + 3.33;
+
+            lstBox.Items.Add(distance);
+
+            //slotAnimation.From = distance - 10;
+            animation_from = animation_from ?? slotAnimation.From;
+            var animation_to = animation_from + (distance * 13);
+
+            slotAnimation.To = animation_to;
+            slotAnimation.Duration = new Duration(TimeSpan.FromSeconds(duration));
+            sb.Begin();
+
+            animation_from = animation_to;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            sb.Stop();
+        }
+        private void SlotAnimation_Completed(object sender, EventArgs e)
+        {
+            //lstBox.Items.Add(llb.Items.CurrentItem);
+        }
+    }
+}
